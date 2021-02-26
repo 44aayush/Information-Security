7c9c5ed91d446783bc83cf64a443d1afbc9bd2fc
44aayush <47076220+44aayush@users.noreply.github.com>
2021-02-26 16:46:30 +0530

    Update infinite.sh

--- LAB 3 - BOF/infinite.sh
+++ LAB 3 - BOF/infinite.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SECONDS=0
 value=0
-while[1]
+while [ 1 ]
 do
 value=$(($value+1))
 duration=$SECONDS
@@ -9,4 +9,5 @@ min=$(($duration/60))
 sec=$(($duration%60))
 echo "$min minutes and $sec seconds elapsed."
 echo "The program has been running $value times so far."
- 
+ ./stack
+ done
